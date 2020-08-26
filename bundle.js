5d8db9d6b7f4f4407c55e934fd0500a65022efa2
yuuangzhang <yuuangzhang@tencent.com>
2020-08-26 19:15:49 +0800

    init

---
+++ bundle.js
@@ -0,0 +1,39 @@
+function getDefaultExportFromCjs (x) {
+	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
+}
+
+function createCommonjsModule(fn, basedir, module) {
+	return module = {
+	  path: basedir,
+	  exports: {},
+	  require: function (path, base) {
+      return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
+    }
+	}, fn(module, module.exports), module.exports;
+}
+
+function commonjsRequire () {
+	throw new Error('Dynamic requires are not currently supported by @rollup/plugin-commonjs');
+}
+
+const foo = 'foo';
+var foo_2 = foo;
+
+var foo_1 = {
+	foo: foo_2
+};
+
+var rollupErrorRepo = createCommonjsModule(function (module, exports) {
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+exports.__esModule = true;
+
+__export(foo_1);
+
+// //Dummy change to keep lerna happy
+});
+
+var index = /*@__PURE__*/getDefaultExportFromCjs(rollupErrorRepo);
+
+export default index;
